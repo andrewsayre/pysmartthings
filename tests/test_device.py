d428694ff79096e7ce97fb7a7e150e1b387e1188
andrewsayre <6730289+andrewsayre@users.noreply.github.com>
2018-12-04 23:28:28 -0600

    Tests for Device class

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,7 +1,9 @@
 """Tests for the Device file."""
 
+# pylint: disable=missing-docstring
+
 import json
-import pysmartthings
+from pysmartthings.smartthings import Device
 
 
 class TestDevice:
@@ -13,9 +15,43 @@ class TestDevice:
     def test_initialize():
         """Tests whether the Device class initializes correctly."""
         # arrange
-        with open("tests/json/device.json", "r") as json_file:
-            entity = json.load(json_file)
+        entity = TestDevice._get_device_entity()
         # act
-        device = pysmartthings.smartthings.Device(None, entity)
+        device = Device(None, entity)
         # assert
         assert device.device_id == "743de49f-036f-4e9c-839a-2f89d57607db"
+        assert device.name == "GE In-Wall Smart Dimmer"
+        assert device.label == "Front Porch Lights"
+        assert device.location_id == "397678e5-9995-4a39-9d9f-ae6ba310236b"
+        assert device.type == "DTH"
+        assert device.device_type_id == "8a9d4b1e3b9b1fe3013b9b206a7f000d"
+        assert device.device_type_name == "Dimmer Switch"
+        assert device.device_type_network == "ZWAVE"
+        assert device.capabilities == [
+            "switch", "switchLevel", "refresh", "indicator",
+            "sensor", "actuator", "healthCheck", "light"]
+
+    @staticmethod
+    def test_update():
+        """Tests updating the status of the device."""
+        # arrange
+        api = TestDevice.MockAPI()
+        device = Device(api, TestDevice._get_device_entity())
+        # act
+        device.update()
+        # assert
+        assert device.status == {"light": "off",
+                                 "switchLevel": 46, "switch": "off"}
+
+    @staticmethod
+    def _get_device_entity():
+        with open("tests/json/device.json", "r") as json_file:
+            return json.load(json_file)
+
+    class MockAPI:
+        # pylint: missing-docstring
+        @staticmethod
+        def get_device_status(device_id):
+            # pylint: disable=unused-argument
+            with open("tests/json/device_main_status.json", "r") as json_file:
+                return json.load(json_file)
