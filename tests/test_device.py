f82bf5ef7db251be18a01b9294d322c9cad08f16
andrewsayre <6730289+andrewsayre@users.noreply.github.com>
2018-12-04 22:50:40 -0600

    Added test linting and additional environment testing

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,12 +1,21 @@
-import pysmartthings
+"""Tests for the Device file."""
+
 import json
+import pysmartthings
 
 
-class Test_Device(object):
-    def test_initialize(self):
+class TestDevice:
+    """Tests for the Device class."""
+
+    # pylint: disable=too-few-public-methods
+
+    @staticmethod
+    def test_initialize():
+        """Tests whether the Device class initializes correctly."""
+        # arrange
         with open("tests/json/device.json", "r") as json_file:
             entity = json.load(json_file)
-
+        # act
         device = pysmartthings.smartthings.Device(None, entity)
-
+        # assert
         assert device.device_id == "743de49f-036f-4e9c-839a-2f89d57607db"
