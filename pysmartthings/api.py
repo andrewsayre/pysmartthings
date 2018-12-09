96baab4b8fe592a0ee643857775ed44c62792a9c
andrewsayre <6730289+andrewsayre@users.noreply.github.com>
2018-12-09 14:23:01 -0600

    Better error handling in the API

--- pysmartthings/api.py
+++ pysmartthings/api.py
@@ -1,15 +1,14 @@
 """Utility for invoking the SmartThings Cloud API."""
 
-import logging
+from typing import Sequence
 import requests
+from . import errors
 
 API_BASE: str = 'https://api.smartthings.com/v1/'
 API_RESOURCE_LOCATIONS = "locations"
 API_RESOURCE_DEVICES: str = "devices"
 API_RESOURCE_DEVICE_STATUS: str = "devices/{device_id}/components/main/status"
 API_RESOURCE_DEVICE_COMMAND: str = "devices/{device_id}/commands"
-
-_LOGGER = logging.getLogger(__name__)
 
 
 class API:
@@ -19,33 +18,34 @@ class API:
     https://smartthings.developer.samsung.com/docs/api-ref/st-api.html
     """
 
-    def __init__(self, token):
+    def __init__(self, token: str):
         """Initialize a new instance of the API class."""
         self._headers = {"Authorization": "Bearer " + token}
 
-    def get_locations(self):
+    def get_locations(self) -> Sequence[dict]:
         """
         Get locations.
 
         https://smartthings.developer.samsung.com/docs/api-ref/st-api.html#operation/listLocations
         """
-        return self._get_request(API_RESOURCE_LOCATIONS)
+        return self._make_request('get', API_RESOURCE_LOCATIONS)
 
-    def get_devices(self):
+    def get_devices(self) -> Sequence[dict]:
         """
         Get the device definitions.
 
         https://smartthings.developer.samsung.com/docs/api-ref/st-api.html#operation/getDevices
         """
-        return self._get_request(API_RESOURCE_DEVICES)
+        return self._make_request('get', API_RESOURCE_DEVICES)
 
-    def get_device_status(self, device_id):
+    def get_device_status(self, device_id: str) -> dict:
         """Get the status of a specific device."""
-        return self._get_request(
+        return self._make_request(
+            'get',
             API_RESOURCE_DEVICE_STATUS.format(device_id=device_id))
 
     def post_command(self, device_id, capability, command, args,
-                     component="main"):
+                     component="main") -> object:
         """
         Execute commands on a device.
 
@@ -63,29 +63,22 @@ class API:
         if args:
             data["commands"][0]["arguments"] = args
 
-        return self._post_request(
-            API_RESOURCE_DEVICE_COMMAND.format(device_id=device_id), data)
+        return self._make_request(
+            'post',
+            API_RESOURCE_DEVICE_COMMAND.format(device_id=device_id),
+            data)
 
-    def _get_request(self, resource):
-        try:
-            resp = requests.get(API_BASE + resource, headers=self._headers)
-            if resp.content:
-                return resp.json()
-            return True
-        except requests.exceptions.RequestException as exception:
-            _LOGGER.warning("Exception: %s", exception)
-        return None
+    def _make_request(self, method: str, resource: str, data: dict = None):
+        response = requests.request(
+            method,
+            API_BASE + resource,
+            json=data,
+            headers=self._headers)
 
-    def _post_request(self, resource, data):
-        try:
-            resp = requests.post(
-                API_BASE + resource,
-                json=data,
-                headers=self._headers)
-            if resp.ok:
-                if resp.content:
-                    return resp.json()
-                return True
-        except requests.exceptions.RequestException as exception:
-            _LOGGER.warning("Exception: %s", exception)
-        return None
+        if response.ok:
+            return response.json()
+        if response.status_code == 401:
+            raise errors.APIUnauthorizedError
+        elif response.status_code == 403:
+            raise errors.APIForbiddenError
+        raise errors.APIUnknownError
---
+++ pysmartthings/errors.py
@@ -0,0 +1,33 @@
+"""Define errors that can be returned from the SmartThings API."""
+
+UNAUTHORIZED_ERROR = \
+    "Authorization for the API is required, but the request has not been " \
+    "authenticated."
+FORBIDDEN_ERROR = \
+    "The request has been authenticated but does not have appropriate" \
+    "permissions, or a requested resource is not found."
+UNKNOWN_ERROR = "An unknown API error occurred."
+
+
+class APIUnauthorizedError(Exception):
+    """Define the error when the API key is invalid."""
+
+    def __init__(self):
+        """Initialize instance of APIUnauthorizedError."""
+        Exception.__init__(self, UNAUTHORIZED_ERROR)
+
+
+class APIForbiddenError(Exception):
+    """Define the error when the API key does not have permission."""
+
+    def __init__(self):
+        """Initialize instance of APIForbiddenError."""
+        Exception.__init__(self, FORBIDDEN_ERROR)
+
+
+class APIUnknownError(Exception):
+    """Define a general error condition."""
+
+    def __init__(self):
+        """Initialize instance of APIUnknownError."""
+        Exception.__init__(self, UNKNOWN_ERROR)
