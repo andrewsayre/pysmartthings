f13f8ab9147a026e4514af1d3c701cd0909a62f6
Andrew Sayre <andrew@sayre.net>
2020-03-28 15:21:23 -0500

    Remove 3.5 support classification

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
         "Topic :: Home Automation",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
